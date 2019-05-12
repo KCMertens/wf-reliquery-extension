81858a084e8ac2ac3a45f847eaeee3c63b63ef4d
KCMertens <koen.mmmm@gmail.com>
2019-05-12 19:16:24 +0200

    Add all known dupes

--- src/config/dupes.ts
+++ src/config/dupes.ts
@@ -11,8 +11,62 @@ export const dupes: {
         "Akjagara Prime Barrel",
         "Akjagara Prime Receiver",
     ],
+    "Akstiletto Prime": [
+        "Akstiletto Prime Barrel",
+        "Akstiletto Prime Receiver"
+    ],
+    "Hikou Prime": [
+        "Hikou Prime Pouch",
+        "Hikou Prime Stars"
+    ],
+    "Spira Prime": [
+        "Spira Prime Blade",
+        "Spira Prime Pouch"
+    ],
+    "Ankyros Prime": [
+        "Ankyros Prime Blade",
+        "Ankyros Prime Gauntlet"
+    ],
+    "Bo Prime": [
+        "Bo Prime Ornament"
+    ],
+    "Dual Kamas Prime": [
+        "Dual Kamas Prime Blade",
+        "Dual Kamas Prime Handle"
+    ],
+    "Fang Prime": [
+        "Fang Prime Blade",
+        "Fang Prime Handle"
+    ],
+    "Glaive Prime": [
+        "Glaive Prime Blade"
+    ],
+    "Kogake Prime": [
+        "Kogake Prime Boot",
+        "Kogake Prime Gauntlet"
+    ],
+    "Kronen Prime": [
+        "Kronen Prime Blade",
+        "Kronen Prime Handle"
+    ],
+    "Nami Skyla Prime": [
+        "Nami Skyla Prime Blade",
+        "Nami Skyla Prime Handle"
+    ],
+    "Orthos Prime": [
+        "Orthos Prime Blade"
+    ],
+    "Tipedo Prime": [
+        "Tipedo Prime Ornament"
+    ],
+    "Venka Prime": [
+        "Venka Prime Blades",
+        "Venka Prime Gauntlet"
+    ],
 };
 
 export const dupeSets: BlueprintSet[] = [
     "Bronco Prime",
+    "Lex Prime",
+    "Vasto Prime",
 ];
